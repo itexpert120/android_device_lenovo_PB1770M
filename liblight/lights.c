778b2069e5619ad24f88c59eff39824658d54a67
Tipz Team <47551974+TipzTeam@users.noreply.github.com>
2020-07-31 09:50:12 +0800

    Update lights.c

--- liblight/lights.c
+++ liblight/lights.c
@@ -312,7 +312,7 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
     .version_major = 1,
     .version_minor = 0,
     .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "Lenovo zoom Lights Module",
+    .name = "Lenovo PB1770M Lights Module",
     .author = "The CyanogenMod Project",
     .methods = &lights_module_methods,
 };
